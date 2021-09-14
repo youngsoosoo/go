60b257dfb45ee1cc0897f6d07b818fa0db2bfe50
Young Soo <lovepys01@naver.com>
2021-09-14 17:35:10 +0900

    난수 추출된 수의 소수 판정 프로그램 v0.4

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time" // seed 생성용 패키지
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.3
+// 난수 추출된 수의 소수 판정 프로그램 v0.4
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
@@ -15,19 +15,50 @@ func main() {
 	number := rand.Intn(150) + 2
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 2; i <= number; i++ { // 1과 number일때 loop 돌지 않음
+	for i := 2; i <= number; i++ {
 		if number%i == 0 {
 			isPrime = false
-			//count = count + 1
 		}
+		fmt.Print(i, " ")
 	}
 
-	if isPrime == true {
+	if isPrime { // 비교 연산자 제거
 		fmt.Println(number, "는(은) 소수입니다!")
 	} else {
 		fmt.Println(number, "는(은) 소수가 아닙니다~")
 	}
 }
+
+//package main
+//
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time" // seed 생성용 패키지
+//)
+//
+//// 난수 추출된 수의 소수 판정 프로그램 v0.3
+//func main() {
+//	seed := time.Now().Unix()
+//	rand.Seed(seed)
+//
+//	isPrime := true
+//	number := rand.Intn(150) + 2
+//	fmt.Println("임의로 추출된 수 : ", number)
+//
+//	for i := 2; i <= number; i++ { // 1과 number일때 loop 돌지 않음
+//		if number%i == 0 {
+//			isPrime = false
+//			//count = count + 1
+//		}
+//	}
+//
+//	if isPrime == true {
+//		fmt.Println(number, "는(은) 소수입니다!")
+//	} else {
+//		fmt.Println(number, "는(은) 소수가 아닙니다~")
+//	}
+//}
 
 //package main
 //
